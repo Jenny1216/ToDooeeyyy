c5e7cd0664d693f891ad1aae24a56d3e2afdf7b4
Jinisha Savani <jinisha@jinishas-macbook-pro.local>
2018-08-13 14:13:25 -0400

    Add new item functionality complete

--- ToDooeeyyy/ToDoListVC.swift
+++ ToDooeeyyy/ToDoListVC.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ToDoListVC: UITableViewController {
     
-    let itemArray = ["item 1", "item 2", "item 3"]
+    var itemArray = ["item 1", "item 2", "item 3"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,12 +48,35 @@ class ToDoListVC: UITableViewController {
         else {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
-        
-        tableView.deselectRow(at: indexPath, animated: true)
-        
+         tableView.deselectRow(at: indexPath, animated: true)
         
     }
+    
+    //MARK- Add New Items
 
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoeeyy Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            // what will happen once the user clicks the Add Item button on our UIAlert
+            
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
 }
 
 
